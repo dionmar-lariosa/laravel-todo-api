5ee95bc29b69fb974e2840a83597c7c543ff9dc7
dionmar-lariosa <main.dionmar@gmail.com>
2023-06-21 00:31:30 +0700

    register a user

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\HttpResponses;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\StoreUserRequest;
 
 class AuthController extends Controller
 {
@@ -14,9 +17,21 @@ class AuthController extends Controller
         return 'this is login';
     }
 
-    public function register()
+    public function register(StoreUserRequest $request)
     {
-        return 'this is register';
+        $request->validated($request->only(['name', 'email', 'password']));
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // trait success
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token')->plainTextToken
+        ]);
     }
 
     public function logout()
